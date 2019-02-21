118b6fdfac36f3b80cea993b929a788454939add
stella2302 <stella2302@users.noreply.github.com>
2019-02-21 21:30:45 +0100

    kodet er ryddet op nu og alt er nice

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
 var dropdown = document.querySelector(".dropdownItems");
 
-var container = document.querySelector(".container");
-
-//BURGER ICON WHEN NOTHING IS CLICKED
 var burgerIcon = document.querySelector(".burgerIcon");
-
-//BURGER ICON WHEN THE BURGER MENU IS CLICKED
-var burgerIcon2 = document.querySelector(".burgerIcon2");
-
-var header = document.querySelector(".header");
 
 var body = document.querySelector("body");
 
@@ -19,14 +11,12 @@ burgerIcon.addEventListener("click", function () {
     if (!(dropdown.style.display == "block")) {
         console.log("dropdown shown");
         dropdown.style.display = "block";
-        container.style.display = "none";
 
         //ADD CLASS "gradientBody" TO BODY
         body.classList.add("gradientBody");
     } else { //IF MENU IS SHOWN, THEN HIDE
         console.log("dropdown hidden");
         dropdown.style.display = "none";
-        container.style.display = "block";
 
         //REMOVE CLASS "gradientBody" FROM BODY
         body.classList.remove("gradientBody");
