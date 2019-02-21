4df041c59eed0d6036e58a53400efd19ad8ba26f
stella2302 <stella2302@users.noreply.github.com>
2019-02-21 18:41:37 +0100

    burgermenu nu med gradient på

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ var dropdown = document.querySelector(".dropdownItems");
 
 var container = document.querySelector(".container");
 
+//BURGER ICON WHEN NOTHING IS CLICKED
 var burgerIcon = document.querySelector(".burgerIcon");
+
+//BURGER ICON WHEN THE BURGER MENU IS CLICKED
+var burgerIcon2 = document.querySelector(".burgerIcon2");
+
+var header = document.querySelector(".header");
+
+var body = document.querySelector("body");
 
 
 //EVENTLISTENER ON HAMBURGER ICON
@@ -12,9 +20,15 @@ burgerIcon.addEventListener("click", function () {
         console.log("dropdown shown");
         dropdown.style.display = "block";
         container.style.display = "none";
+
+        //ADD CLASS "gradientBody" TO BODY
+        body.classList.add("gradientBody");
     } else { //IF MENU IS SHOWN, THEN HIDE
         console.log("dropdown hidden");
         dropdown.style.display = "none";
         container.style.display = "block";
+
+        //REMOVE CLASS "gradientBody" FROM BODY
+        body.classList.remove("gradientBody");
     }
 });
